5c3d46a4bbabddeab26415699de99ee84778ef70
HiddenRoom <bestwitherskele@gmail.com>
2022-12-13 11:56:39 -0800

    improved clarity with floor instead of int cast and extended math

--- src/renderMath.c
+++ src/renderMath.c
@@ -5,21 +5,32 @@
 double rayCollisonDist(char **map, double cameraX, double cameraY, double rayAngle)
 {
   const double cameraXInit = cameraX;
-  const cameraYIniti = cameraY;
+  const double cameraYInit = cameraY;
 
   double horiXOffset, horiYOffset, horiHypo;
   double veriXOffset, vertYOffset, veriHypo;
 
-  while(!(map[(int)cameraX][(int)cameraY]))
+  while(!(map[floor(cameraX)][floor(cameraY)]))
   {
-    horiXOffset = 1.0f - (cameraX - (int)cameraX);
+    horiXOffset = 1.0f - (cameraX - floor(cameraX));
     horiYOffset = tan(rayAngle) * horiXOffset;
     horiHypo = sqrt(horiXOffset * horiXOffset + horiYOffset * horiYOffset);
 
-    veriYOffset = 1.0f - (cameraY - (int)cameraY);
+    veriYOffset = 1.0f - (cameraY - floor(cameraY));
     veriXOffset = tan(rayAngle) * veriYOffset;
     veriHypo = sqrt(veriXOffset * veriXOffset + veriYOffset * veriYOffset);
 
-    /* add code to update camera pos according to longer hypo */
+    if(horiHypo > veriHypo)
+    {
+      cameraX += horiXOffset;
+      cameraY += horiYOffset;
+    }
+    else
+    {
+      cameraX += veriXOffset;
+      cameraY += veriYOffset;
+    }
   }
+
+  return sqrt(cameraX * cameraX + cameraY * cameraY);
 }
