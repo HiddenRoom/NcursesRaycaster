54567904f6ba9166204b9b07441ba467c3b5ed80
HiddenRoom <bestwitherskele@gmail.com>
2022-12-16 10:21:58 -0800

    add start of test and write actual makefile

--- Makefile
+++ Makefile
@@ -1,2 +1,8 @@
 CFLAGS=-O0 -ggdb3 -g3 -Wall -Wextra
 LINKFLAGS=-lm -lncurses
+
+math: src/renderMath.c src/include/renderMath.h
+	$(CC) $(CFLAGS) -c -o renderMath.o src/renderMath.c $(LINKFLAGS)
+
+test: math src/test.c
+	$(CC) $(CFLAGS) -o test.out src/test.c
---
+++ src/test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "include/renderMath.h"
+
+int main(void)
+{
+  char map[][] = {{1, 1, 1, 1, 1},
+                  {1, 0, 0, 0, 1},
+                  {1, 0, 0, 0, 1},
+                  {1, 0, 0, 0, 1},
+                  {1, 0, 0, 0, 1},
+                  {1, 1, 1, 1, 1}}
+}
