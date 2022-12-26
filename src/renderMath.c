fc5c214bf91a5703307e24ddb020f164f8c7cab8
HiddenRoom <bestwitherskele@gmail.com>
2022-12-25 20:31:43 -0800

    fixed angle adjustment error for tan

--- src/renderMath.c
+++ src/renderMath.c
@@ -1,6 +1,7 @@
 #include "include/renderMath.h"
 
 #include <math.h>
+#include <stdio.h>
  
 double rayCollisonDist(unsigned char **map, int mapXSize, int mapYSize, double cameraX, double cameraY, double rayAngle)
 {
@@ -10,17 +11,17 @@ double rayCollisonDist(unsigned char **map, int mapXSize, int mapYSize, double c
   double horiXOffset, horiYOffset, horiHypo;
   double vertXOffset, vertYOffset, vertHypo;
 
-  while((cameraX < mapXSize && cameraY < mapYSize) && !(map[(int)floor(cameraX)][(int)floor(cameraY)]))
+  while((cameraX < mapXSize && cameraY < mapYSize) && !(map[(int)(floor(cameraX))][(int)floor(cameraY)]))
   {
     horiXOffset = 1.0 - (cameraX - floor(cameraX));
-    horiYOffset = tan((M_PI / 2.0) - rayAngle) * horiXOffset;
+    horiYOffset = tan(rayAngle) * horiXOffset;
     horiHypo = sqrt(horiXOffset * horiXOffset + horiYOffset * horiYOffset);
 
     vertYOffset = 1.0 - (cameraY - floor(cameraY));
     vertXOffset = tan((M_PI / 2.0) - rayAngle) * vertYOffset;
     vertHypo = sqrt(vertXOffset * vertXOffset + vertYOffset * vertYOffset);
 
-    if(horiHypo > vertHypo)
+    if(horiHypo < vertHypo)
     {
       cameraX += horiXOffset;
       cameraY += horiYOffset;
@@ -30,7 +31,11 @@ double rayCollisonDist(unsigned char **map, int mapXSize, int mapYSize, double c
       cameraX += vertXOffset;
       cameraY += vertYOffset;
     }
+
+    printf("camX: %lf\ncamY: %lf\n", cameraX, cameraY);
   }
+
+  printf("final camX: %lf\nfinal camY: %lf\n", cameraX, cameraY);
 
   return sqrt(pow(fabs(cameraXInit - cameraX), 2.0) + pow(fabs(cameraYInit - cameraY), 2.0));
 }
